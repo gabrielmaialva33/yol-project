ebe0a6aaddcede50ab645cfa326c9875e4af8f25
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-04 14:11:25 -0300

    refactor(login): replace hardcoded logo path with import
    Updated the logo path in `LoginPage` to use an imported SVG instead of a hardcoded string. Enhances maintainability and consistency in asset management.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+import logo from '/logo-yol.svg'
 import {LoginForm} from './components/LoginForm'
 
 export function LoginPage() {
@@ -7,11 +8,7 @@ export function LoginPage() {
 			<div className='absolute -left-80 top-[-314px] hidden h-[1102px] w-[1136px] rounded-full border-[15px] border-orange-500/50 shadow-[0_4px_94.6px_13px_#0F172A] md:block' />
 			<div className='container mx-auto flex h-full items-center justify-center px-4 md:justify-between'>
 				<div className='hidden flex-col items-start justify-center gap-5 md:flex'>
-					<img
-						alt='YOL'
-						className='h-auto w-full max-w-[406px]'
-						src='/logo-yol.svg'
-					/>
+					<img alt='YOL' className='h-auto w-full max-w-[406px]' src={logo} />
 				</div>
 				<div className='z-10 w-full max-w-[490px]'>
 					<LoginForm />
