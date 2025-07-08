df10384daabd53d8b470af667ab700c51d52f54b
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-08 01:46:13 -0300

    feat(mocks): add hearings mock data module
    Created a new `hearings.ts` mock data module to support local testing and development. The module contains predefined hearings data for consistent and reliable simulations.

---
+++ src/mocks/data/hearings.ts
@@ -0,0 +1,30 @@
+export const hearings = [
+	{
+		label: 'Audiências',
+		percentage: 50,
+		total: 2,
+		completed: 1,
+		color: '#004B50'
+	},
+	{
+		label: 'Prazos Jud.',
+		percentage: 0,
+		total: 2,
+		completed: 0,
+		color: '#92D7CF'
+	},
+	{
+		label: 'Extra Jud.',
+		percentage: 0,
+		total: 9,
+		completed: 0,
+		color: '#92D7CF'
+	},
+	{
+		label: 'Fatais',
+		percentage: 50,
+		total: 2,
+		completed: 1,
+		color: '#004B50'
+	}
+]
